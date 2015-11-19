22d29bc49768d026a76ba7e0d0b2c17e03cb1364
Wick3r <danielrossner@web.de>
2015-11-19 15:50:54 +0100

    [FIX] added missing ImplementedBy annotation and fixed main template

--- app/modules/authentication/OAuthentication.java
+++ app/modules/authentication/OAuthentication.java
@@ -1,10 +1,13 @@
 package modules.authentication;
 
+import com.google.inject.ImplementedBy;
+
 import java.io.IOException;
 
 /**
  * Created by Daniel on 19.11.2015.
  */
+@ImplementedBy(GoogleAuthentication.class)
 public interface OAuthentication {
 
     String getAuthURL() throws IOException;
