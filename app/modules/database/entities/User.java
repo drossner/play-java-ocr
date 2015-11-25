fcc9843e3bd34d296d70d5d6d4db78022f8b863e
Wick3r <danielrossner@web.de>
2015-11-25 15:10:35 +0100

    [FIX] Hibnerate cascade=All

--- app/modules/database/entities/User.java
+++ app/modules/database/entities/User.java
@@ -23,7 +23,7 @@ public class User implements Subject {
     @Column
     private String eMail;
 
-    @OneToOne
+    @OneToOne(cascade = {CascadeType.ALL})
     private Country country;
 
     @Column
