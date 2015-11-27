762c412cdb1c52d496e00a0e23e9d0ab6e8008d6
Benedikt Linke <benedikt.linke@me.com>
2015-11-27 14:09:08 +0100

    [TASK] added SessionHolder

---
+++ app/modules/cms/SessionHolder.java
@@ -0,0 +1,37 @@
+package modules.cms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.chemistry.opencmis.client.api.Session;
+
+
+/**
+ * Created by Benedikt Linke on 27.11.15.
+ */
+public class SessionHolder {
+
+    Map<String, SessionCMS> sessions = new HashMap<String, SessionCMS>();
+
+    private static SessionHolder instance = null;
+    private SessionHolder() {}
+
+    public static SessionHolder getInstance() {
+        if(instance == null) {
+            instance = new SessionHolder();
+        }
+        return instance;
+    }
+
+    public void storeSession(String sessionName, SessionCMS session) {
+        sessions.put(sessionName,session);
+    }
+
+    public Session retrieveSession(String sessionName) {
+        return (Session) sessions.get(sessionName);
+    }
+
+    void disconnectSession(String sessionName) {
+        sessions.remove(sessionName);
+    }
+}
